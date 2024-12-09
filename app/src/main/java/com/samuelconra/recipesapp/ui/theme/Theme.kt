36ce8cd63e6a79850e0a61e39fca8bca668f0d5d
Samuel Conde <samuelconra@192.168.0.244>
2024-12-09 01:35:36 -0600

    Colors, theme and typo defined

--- app/src/main/java/com/samuelconra/recipesapp/ui/theme/Theme.kt
+++ app/src/main/java/com/samuelconra/recipesapp/ui/theme/Theme.kt
@@ -9,28 +9,35 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    background = DarkModeBackgroundColor,
+    primary = Color.White,
+    tertiary = DarkModeGray,
+    onBackground = DarkModeGray2,
+    onPrimary = DarkModeGray3,
+    onSecondary = DarkModeGray4,
+    onTertiary = CustomGray2,
+    surface = CustomBackgroundColor,
+    surfaceVariant = CustomWhiteOpacity,
+    primaryContainer = CustomBlackOpacity2,
+    inversePrimary = Color.Black,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    background = CustomBackgroundColor,
+    primary = Color.Black,
+    tertiary = Color.White,
+    onBackground = CustomDarkGray,
+    onPrimary = CustomLightGray,
+    onSecondary = CustomGray,
+    onTertiary = CustomGray2,
+    surface = CustomRed,
+    surfaceVariant = CustomBlackOpacity,
+    primaryContainer = CustomBlackOpacity2,
+    inversePrimary = Color.White,
 )
 
 @Composable
@@ -41,18 +48,13 @@ fun RecipesAppTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = poppinsTypo,
         content = content
     )
 }
---
+++ app/src/main/java/com/samuelconra/recipesapp/utils/CustomIcons.kt
@@ -0,0 +1,2 @@
+package com.samuelconra.recipesapp.utils
+
